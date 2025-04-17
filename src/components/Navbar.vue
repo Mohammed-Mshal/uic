4a260e339f412523de58835b9fc1e8969ff3a251
Mohammed-Mshal <83920083+Mohammed-Mshal@users.noreply.github.com>
2025-04-17 09:34:08 +0300

    Edit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import Logo from '../assets/logo.svg'
+</script>
+
+<template>
+    <div class="navbar">
+        <div class="navbar-left">
+            <router-link to="/">
+                <img :src="Logo" alt="Logo" class="logo">
+            </router-link>
+        </div>
+        <div class="navbar-center">
+            <ul>
+                <li><router-link to="/">Home</router-link></li>
+                <li><router-link to="/about">About</router-link></li>
+                <li><router-link to="/contact">Contact</router-link></li>
+            </ul>
+        </div>
+        <div class="navbar-right">
+            
+        </div>
+    </div>
+</template>
